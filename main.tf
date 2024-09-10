0859071718655a42c505b2697e8c93af6e4653b3
Matthew White <74813866+MattWhite-personal@users.noreply.github.com>
2024-09-10 20:46:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "azurerm_network_security_group" "nsg-fail" {
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
-    destination_port_range     = "*"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
