1496e3487855f14397b3e50c232b56649190e24c
Matt White <matt@matthewjwhite.co.uk>
2025-07-25 16:06:11 +0100

    tweak concat

--- module/mtasts/main.tf
+++ module/mtasts/main.tf
@@ -10,7 +10,7 @@ locals {
   }
   front-door-id = var.use-existing-front-door ? data.azurerm_cdn_frontdoor_profile.afd[0].id : azurerm_cdn_frontdoor_profile.mta-sts[0].id
   afd-ip-ranges = flatten([
-    for cidr in data.azurerm_network_service_tags.AzureFrontDoor-BackEnd.ipv4_cidrs : (
+    for cidr in concat(data.azurerm_network_service_tags.AzureFrontDoor-BackEnd.ipv4_cidrs,var.runner-ip) : (
       tonumber(split("/", cidr)[1]) >= 31 ?
       [
         for i in range(
@@ -19,7 +19,7 @@ locals {
       ] :
       [cidr]
     )
-  ], var.runner-ip != "" ? [var.runner-ip] : [])
+  ])
   storage-account-name = "st${local.storage_prefix}mtasts"
 }
 
