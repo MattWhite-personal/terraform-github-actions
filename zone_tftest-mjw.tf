255cf00d5a0f5febb99dae2e12c4429c82da390c
Matt White <matt@matthewjwhite.co.uk>
2025-08-15 22:46:50 +0100

    test a record alias

--- zone_tftest-mjw.tf
+++ zone_tftest-mjw.tf
@@ -8,12 +8,12 @@ resource "azurerm_dns_zone" "tftest-mjw" {
 }
 
 module "tftest-records" {
-  source       = "./module/dnsrecords"
-  zone_name    = azurerm_dns_zone.tftest-mjw.name
-  rg_name      = azurerm_resource_group.dnszones.name
-  tags         = local.tags
-  a-records    = [
-        {
+  source    = "./module/dnsrecords"
+  zone_name = azurerm_dns_zone.tftest-mjw.name
+  rg_name   = azurerm_resource_group.dnszones.name
+  tags      = local.tags
+  a-records = [
+    {
       name    = "@"
       record  = azurerm_cdn_frontdoor_endpoint.static-web-app.id
       isAlias = true
