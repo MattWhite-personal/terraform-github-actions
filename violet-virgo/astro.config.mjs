80cd779aceca08d3e36dd03f6e25b7937a0d3104
Matt White <matt@matthewjwhite.co.uk>
2025-08-19 20:48:19 +0100

    updated website config

--- violet-virgo/astro.config.mjs
+++ violet-virgo/astro.config.mjs
@@ -2,15 +2,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tfttest.matthewjwhite.co.uk',
+	site: 'https://matthewjwhite.co.uk',
 	integrations: [mdx(), sitemap()],
 	markdown: {
+		//syntaxHighlight: 'prism',
 		shikiConfig: {
 			wrap: true,
-			theme: 'github-dark-dimmed',
+			theme: 'slack-dark',
 		}
+		//syntaxHighlight: false,
+    	//rehypePlugins: [[rehypePrettyCode, { theme: "github-light" }]],
 	},
 });
