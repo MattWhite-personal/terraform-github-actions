df18f6a6df8e06ea920adf64f4c56ada259583d5
Matt White <matt@matthewjwhite.co.uk>
2025-08-19 18:50:50 +0100

    tweak domain name pattern

--- violet-virgo/astro.config.mjs
+++ violet-virgo/astro.config.mjs
@@ -2,19 +2,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://matthewjwhite.co.uk',
+	site: 'https://tfttest.matthewjwhite.co.uk',
 	integrations: [mdx(), sitemap()],
 	markdown: {
-		//syntaxHighlight: 'prism',
 		shikiConfig: {
 			wrap: true,
 			theme: 'github-dark-dimmed',
 		}
-		//syntaxHighlight: false,
-    	//rehypePlugins: [[rehypePrettyCode, { theme: "github-light" }]],
 	},
 });
